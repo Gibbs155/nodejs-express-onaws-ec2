90abb16f15d044684baaf4a55850ae88311168e6
Gibbs155 <alejandrob1l@hotmail.com>
2022-04-17 18:23:48 -0500

    fiexd typo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,11 +5,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
+#create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
-
-if [ -d "$DIR"]; then
-    echo "{$DIR} exists"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
 else
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
